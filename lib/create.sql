34b079692503f2bea7d9f9ce69f89db0e44f07a4
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-23 19:06:43 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE projects (
-  id INTEGER PRIMARY KEY, 
-  title TEXT, 
-  category TEXT, 
-  start_date TEXT, 
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
   end_date TEXT);
 
 CREATE TABLE users (name, age);
