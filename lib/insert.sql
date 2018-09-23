226a9cda25ce43e5fb3d949330cc7fbe40207fb3
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-23 19:11:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,16 @@
 INSERT INTO users (name)
-  VALUES ("Joe")
+  VALUES ("Joe");
+INSERT INTO users (name)
+  VALUES ("Bob");
+INSERT INTO users (name)
+  VALUES ("Tod");
+INSERT INTO users (name)
+  VALUES ("Bill");
+INSERT INTO users (name)
+  VALUES ("Will");
+INSERT INTO users (name)
+  VALUES ("Jill");
+INSERT INTO users (name)
+  VALUES ("Burt");
+INSERT INTO users (name)
+  VALUES ("Curt");
